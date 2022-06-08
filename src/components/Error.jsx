9a4ff971dd82042d919082f95c7e0affc3df11fc
Atharva <atharvakarande11@gmail.com>
2022-06-08 16:34:30 +0530

    Created error page.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,26 @@
 import "../css/Error.css";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div>
+      <p className="error__err404">
+        Error <b> 404 !</b>
+      </p>
+      <p className="error__page_not_found">
+        <b>Page Not Found</b>
+      </p>
+      <p className="error__page_not_found__desc">
+        Sorry, but we couldn't find the page you are looking for. Either the
+        page doesn't exist or is temporarily unavailable.
+      </p>
+      <button
+        className="btn btn-outline-secondary btn-lg"
+        onClick={(e) => (window.location.href = "/")}
+      >
+        Return To Homepage
+      </button>
+    </div>
+  );
 };
 
 export default Error;
